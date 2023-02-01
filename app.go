29b9b692e51c6803c862fbf9e841be9e9ba960c7
Timothy <TomeSprout@pm.me>
2023-02-01 09:48:36 -0500

    Add prompt select demo

--- app.go
+++ app.go
@@ -4,10 +4,21 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+
+	"github.com/manifoldco/promptui"
 )
 
 func newLine() {
 	fmt.Print("\n")
+}
+
+func promptSelect(label string, items []string) promptui.Select {
+	prompt := promptui.Select{
+		Label: label,
+		Items: items,
+	}
+
+	return prompt
 }
 
 func main() {
@@ -16,14 +27,29 @@ func main() {
 
 	scanner := bufio.NewScanner(os.Stdin)
 
-	var prompt string = "Send a line"
-	fmt.Println(prompt)
+	var customPrompt string = "Send a line"
+	fmt.Println(customPrompt)
 
 	scanner.Scan()
 	text := scanner.Text()
 	res := "Thanks for the input -> "
 	fmt.Printf("%s%s", res, text)
 	newLine()
-}
 
-// Don't forget to run command --> `go mod tidy`
+	label := "Choose something"
+	items := []string{
+		"JP",
+		"CN",
+	}
+
+	prompt := promptSelect(label, items)
+	_, result, err := prompt.Run()
+
+	if err != nil {
+		fmt.Printf("prompt failed -- %v\n", err)
+		return
+	}
+
+	fmt.Printf("You choose %q\n", result)
+
+}
